2a5d42634512dfc58f41ab13f9fbaf34c66a3379
kato-toranosuke <toranosuke11041999@gmail.com>
2021-06-21 15:12:56 +0000

    [Fix] update travis ci

--- test/travis_ros_install.bash
+++ test/travis_ros_install.bash
@@ -8,15 +8,15 @@ pip3 install rospkg
 
 #ros install
 cd ..
-git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu20.04_server.git
-cd ./ros_setup_scripts_Ubuntu20.04_server
+git clone https://github.com/ryuichiueda/ros_setup_scripts_Ubuntu14.04_server.git
+cd ./ros_setup_scripts_Ubuntu14.04_server
 bash ./step0.bash
 bash ./step1.bash
 
 #catkin setup
 mkdir -p ~/catkin_ws/src
 cd ~/catkin_ws/src
-source /opt/ros/noetic/setup.bash
+source /opt/ros/indigo/setup.bash
 catkin_init_workspace
 cd ~/catkin_ws
 catkin_make
